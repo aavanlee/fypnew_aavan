4c3b2bd827bc6c58710e48d75ac3f5a9eb754705
aavanlee <77765657+aavanlee@users.noreply.github.com>
2021-06-15 20:58:50 +0800

    update working

--- apistart.py
+++ apistart.py
@@ -7,6 +7,8 @@ ALLOWED_EXTENSIONS = {'mp4'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = '12345'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -18,12 +20,14 @@ def upload_file():
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
+            print("No file part")
             return redirect(request.url)
         file = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
             flash('No selected file')
+            print("No selected file")
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
